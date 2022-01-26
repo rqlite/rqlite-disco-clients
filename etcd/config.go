f18bf9a401b09f3d84dd7cc36abf425571307a03
Philip O'Toole <philip.otoole@yahoo.com>
2022-01-26 09:30:19 -0500

    Add example etcd config

--- etcd/config.go
+++ etcd/config.go
@@ -4,6 +4,22 @@ import (
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
+const (
+	// exampleConfig is an example of how the etcd config file
+	// should be structured.
+	exampleConfig = `
+{
+	"endpoints": ["http://1.2.3.4:8080", "https://5.6.7.8"],
+	"auto-sync-interval": 10000,
+	"dial-timeout": 30000,
+	"dial-keep-alive-timeout": 900000,
+	"username": "me",
+	"password": "my password",
+	"reject-old-cluster": true
+}
+`
+)
+
 // Config stores the configuration for the etcd client.
 // The full definition is available at https://pkg.go.dev/go.etcd.io/etcd/clientv3#Config
 type Config clientv3.Config
---
+++ etcd/config_test.go
@@ -0,0 +1,17 @@
+package etcd
+
+import (
+	"strings"
+	"testing"
+)
+
+func Test_LoadExampleConfig(t *testing.T) {
+	r := strings.NewReader(exampleConfig)
+	cfg, err := NewConfigFromReader(r)
+	if err != nil {
+		t.Fatalf("failed to generate config: %s", err.Error())
+	}
+	if cfg == nil {
+		t.Fatalf("nil config")
+	}
+}
