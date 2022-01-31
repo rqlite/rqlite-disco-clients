eb2c36c4becf1a1a52a1a2a2d6d86fe4a942dcf7
Philip O'Toole <philip.otoole@yahoo.com>
2022-01-31 00:30:49 -0500

    Add DNS SRV support

---
+++ dnssrv/client.go
@@ -0,0 +1,118 @@
+package dnssrv
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+	"sync"
+	"time"
+)
+
+// Client is a type can retrieve SRV records for rqlite
+type Client struct {
+	name    string
+	service string
+
+	mu            sync.Mutex
+	lastContact   time.Time
+	lastAddresses []string
+	lastError     error
+
+	// Can be explicitly set for test purposes.
+	lookupSRVFn func(service, proto, name string) (string, []*net.SRV, error)
+	lookupFn    func(host string) ([]net.IP, error)
+}
+
+// NewConfigFromReader returns a Client configuration from the data read
+// from r. If r is nil, a nil Configuration is returned.
+func NewConfigFromReader(r io.Reader) (*Config, error) {
+	if r == nil {
+		return nil, nil
+	}
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	if err := json.Unmarshal(b, &cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+// New returns an instantiated DNS SRV client. If the cfg is nil, the default
+// config is used.
+func New(cfg *Config) *Client {
+	client := &Client{
+		name:        "rqlite",
+		service:     "rqlite",
+		lookupSRVFn: net.LookupSRV,
+		lookupFn:    net.LookupIP,
+	}
+
+	if cfg != nil {
+		if cfg.Name != "" {
+			client.name = cfg.Name
+		}
+		if cfg.Service != "" {
+			client.service = cfg.Service
+		}
+	}
+	return client
+}
+
+// Lookup returns the network addresses from the DNS SRV records
+func (c *Client) Lookup() ([]string, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	var records []*net.SRV
+	_, records, c.lastError = c.lookupSRVFn(c.service, "tcp", c.name)
+	if c.lastError != nil {
+		return nil, c.lastError
+	}
+	c.lastContact = time.Now()
+
+	addrs := make([]string, 0)
+	for i := range records {
+		// Now look up the IP address for the target. If there are more than
+		// one, add them all.
+		var ips []net.IP
+		ips, c.lastError = c.lookupFn(records[i].Target)
+		if c.lastError != nil {
+			return nil, c.lastError
+		}
+
+		for j := range ips {
+			addrs = append(addrs, fmt.Sprintf("%s:%d", ips[j].String(), records[i].Port))
+		}
+	}
+
+	c.lastAddresses = make([]string, len(addrs))
+	copy(c.lastAddresses, addrs)
+
+	return addrs, nil
+}
+
+// Stats returns some basic diagnostics information about the client.
+func (c *Client) Stats() (map[string]interface{}, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	stats := map[string]interface{}{
+		"mode":    "dnssrv",
+		"name":    c.name,
+		"service": c.service,
+	}
+
+	if c.lastError != nil {
+		stats["last_error"] = c.lastError.Error()
+	}
+	if !c.lastContact.IsZero() {
+		stats["last_contact"] = c.lastContact
+		stats["last_addresses"] = c.lastAddresses
+	}
+
+	return stats, nil
+}
---
+++ dnssrv/client_test.go
@@ -0,0 +1,98 @@
+package dnssrv
+
+import (
+	"net"
+	"reflect"
+	"testing"
+)
+
+func Test_NewClient(t *testing.T) {
+	client := New(nil)
+	if client == nil {
+		t.Fatalf("failed to create default client")
+	}
+}
+
+func Test_ClientStats(t *testing.T) {
+	client := New(nil)
+	stats, err := client.Stats()
+	if err != nil {
+		t.Fatalf("failed to get stats: %s", err.Error())
+	}
+	if stats == nil {
+		t.Fatalf("nil stats returned")
+	}
+}
+
+func Test_ClientLookupSingle(t *testing.T) {
+	client := New(nil)
+
+	lookupSRVFn := func(service, proto, name string) (string, []*net.SRV, error) {
+		if exp, got := "rqlite", service; exp != got {
+			t.Fatalf("incorrect service resolved, exp %s, got %s", exp, got)
+		}
+		if exp, got := "rqlite", name; exp != got {
+			t.Fatalf("incorrect name resolved, exp %s, got %s", exp, got)
+		}
+		return "", []*net.SRV{{"rqlite.node", 1000, 1, 10}}, nil
+	}
+	client.lookupSRVFn = lookupSRVFn
+
+	lookupFn := func(host string) ([]net.IP, error) {
+		if exp, got := "rqlite.node", host; exp != got {
+			t.Fatalf("incorrect host resolved, exp %s, got %s", exp, got)
+		}
+		return []net.IP{net.IPv4(8, 8, 8, 8)}, nil
+	}
+	client.lookupFn = lookupFn
+
+	addrs, err := client.Lookup()
+	if err != nil {
+		t.Fatalf("failed to lookup SRV record: %s", err.Error())
+	}
+	if exp, got := 1, len(addrs); exp != got {
+		t.Fatalf("wrong number of addresses returned, exp %d, got %d", exp, got)
+	}
+	if !reflect.DeepEqual(addrs, []string{"8.8.8.8:1000"}) {
+		t.Fatalf("failed to get correct address: %s", addrs)
+	}
+}
+
+func Test_ClientLookupDouble(t *testing.T) {
+	client := New(nil)
+	client.name = "rqlite-name"
+	client.service = "rqlite-service"
+
+	lookupSRVFn := func(service, proto, name string) (string, []*net.SRV, error) {
+		if exp, got := "rqlite-service", service; exp != got {
+			t.Fatalf("incorrect service resolved, exp %s, got %s", exp, got)
+		}
+		if exp, got := "rqlite-name", name; exp != got {
+			t.Fatalf("incorrect name resolved, exp %s, got %s", exp, got)
+		}
+		return "", []*net.SRV{{"rqlite.node.1", 1000, 1, 10}, {"rqlite.node.2", 2000, 1, 10}}, nil
+	}
+	client.lookupSRVFn = lookupSRVFn
+
+	lookupFn := func(host string) ([]net.IP, error) {
+		if host == "rqlite.node.1" {
+			return []net.IP{net.IPv4(1, 1, 1, 1)}, nil
+		} else if host == "rqlite.node.2" {
+			return []net.IP{net.IPv4(2, 2, 2, 2)}, nil
+		}
+		t.Fatalf("incorrect host resolved, got %s", host)
+		return nil, nil
+	}
+	client.lookupFn = lookupFn
+
+	addrs, err := client.Lookup()
+	if err != nil {
+		t.Fatalf("failed to lookup SRV record: %s", err.Error())
+	}
+	if exp, got := 2, len(addrs); exp != got {
+		t.Fatalf("wrong number of addresses returned, exp %d, got %d", exp, got)
+	}
+	if !reflect.DeepEqual(addrs, []string{"1.1.1.1:1000", "2.2.2.2:2000"}) {
+		t.Fatalf("failed to get correct address: %s", addrs)
+	}
+}
